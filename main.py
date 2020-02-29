ad246964bcf9561959344b7d5fbfdd1026c6798f
renma <titech.renma@gmail.com>
2020-02-29 19:47:45 +0900

    Functionality is completed and refactoring is done.

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import dlib
 import cv2
 import numpy as np
 
-cv2.destroyAllWindows()
-
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+STRAIGHT = 0
+LEFT = 1
+RIGHT = 2
 
 def get_center(gray_img):
     moments = cv2.moments(gray_img, False)
@@ -14,103 +16,101 @@ def get_center(gray_img):
     except:
         return None
 
-def is_close(y0, y1):
-    if abs(y0 - y1) < 10:
-        return True
-    return False
-
 def matrix2line(arr):
     m = np.shape(arr)[0]
     n = np.shape(arr)[1]
     return arr.reshape(m*n,1)
 
-def eye_point(img, parts, left=True):
-    if left:
-        eyes = [
-                parts[36],
-                min(parts[37], parts[38], key=lambda x: x.y),
-                max(parts[40], parts[41], key=lambda x: x.y),
-                parts[39],
-                ]
-    else:
-        eyes = [
-                parts[42],
-                min(parts[43], parts[44], key=lambda x: x.y),
-                max(parts[46], parts[47], key=lambda x: x.y),
-                parts[45],
-                ]
-    if is_close(eyes[1].y, eyes[2].y):
-        return None
-
-    eye = img[eyes[1].y:eyes[2].y, eyes[0].x:eyes[3].x]
-    imgGrayLineData = matrix2line(cv2.cvtColor(eye, cv2.COLOR_RGB2GRAY))
-    threshVal = np.median(imgGrayLineData)
-    _, eye = cv2.threshold(cv2.cvtColor(eye, cv2.COLOR_RGB2GRAY), threshVal, 255, cv2.THRESH_BINARY_INV)
-
-    center = get_center(eye)
-    if center:
-        return center[0] + eyes[0].x, center[1] + eyes[1].y
-    return center
-
-
-def calc_black_whiteArea(bw_image):
+def calc_white_area(bw_image):
     image_size = bw_image.size
-    whitePixels = cv2.countNonZero(bw_image)
-    whiteAreaRatio = (whitePixels/image_size)*100#[%]
+    white_pixels = cv2.countNonZero(bw_image)
+    white_area_ratio = (white_pixels/image_size)*100#[%]
 
-    return whiteAreaRatio
+    return white_area_ratio
 
-def eye_ratio(img, parts, left=True):
+def get_median_value_from_img(img):
+    img_pixel_data = matrix2line(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY))
+    return np.median(img_pixel_data)
+
+def is_eye_close(y0, y1):
+    if abs(y0 - y1) < 10:
+        return True
+    return False
+
+def get_eye_parts(parts, left):
     if left:
-        eyes = [
+        eye_parts = [
                 parts[36],
                 min(parts[37], parts[38], key=lambda x: x.y),
                 max(parts[40], parts[41], key=lambda x: x.y),
                 parts[39],
                 ]
     else:
-        eyes = [
+        eye_parts = [
                 parts[42],
                 min(parts[43], parts[44], key=lambda x: x.y),
                 max(parts[46], parts[47], key=lambda x: x.y),
                 parts[45],
                 ]
-    if is_close(eyes[1].y, eyes[2].y):
+
+    if is_eye_close(eye_parts[1].y, eye_parts[2].y):
+        return None
+    else:
+        return eye_parts
+
+def get_eye_center(img, parts, left=True):
+    eye_parts = get_eye_parts(parts, left)
+    if eye_parts == None:
         return None
 
-    redge_x = eyes[0].x
-    ledge_x = eyes[3].x
-    eye = img[eyes[1].y:eyes[2].y, eyes[0].x:eyes[3].x]
-    eye_left_part = img[eyes[1].y:eyes[2].y, (2*ledge_x + redge_x)//3:ledge_x]
-    eye_right_part = img[eyes[1].y:eyes[2].y, redge_x:(ledge_x + 2*redge_x)//3]
+    eye_img = img[eye_parts[1].y:eye_parts[2].y, eye_parts[0].x:eye_parts[3].x]
+    _, eye_img = cv2.threshold(cv2.cvtColor(eye_img, cv2.COLOR_RGB2GRAY), get_median_value_from_img(eye_img), 255, cv2.THRESH_BINARY_INV)
 
-    imgGrayLineData = matrix2line(cv2.cvtColor(eye, cv2.COLOR_RGB2GRAY))
-    threshVal = np.median(imgGrayLineData)
-
-    _, eye = cv2.threshold(cv2.cvtColor(eye, cv2.COLOR_RGB2GRAY), threshVal, 255, cv2.THRESH_BINARY_INV)
-    _, eye_left_part = cv2.threshold(cv2.cvtColor(eye_left_part, cv2.COLOR_RGB2GRAY), threshVal, 255, cv2.THRESH_BINARY_INV)
-    _, eye_right_part = cv2.threshold(cv2.cvtColor(eye_right_part, cv2.COLOR_RGB2GRAY), threshVal, 255, cv2.THRESH_BINARY_INV)
-
-    """
-    if left==True:
-        cv2.imshow("left_eye", eye)
-    if left==False:
-        cv2.imshow("right_eye", eye)
-    """
-
-    threshBW = 80
-    if calc_black_whiteArea(eye_left_part) > threshBW:
-        return 1
-    elif calc_black_whiteArea(eye_right_part) > threshBW:
-        return 2
+    center = get_center(eye_img)
+    if center:
+        return center[0] + eye_parts[0].x, center[1] + eye_parts[1].y
     else:
-        return 0
+        return center
 
-def p(img, parts, eye):
-    if eye[0]:
-        cv2.circle(img, eye[0], 3, (255, 255, 0), -1)
-    if eye[1]:
-        cv2.circle(img, eye[1], 3, (255, 255, 0), -1)
+def eye_moving_direction(img, parts, left=True):
+    eye_parts = get_eye_parts(parts, True)
+    if eye_parts == None:
+        return None
+
+    eye_left_edge = eye_parts[3].x
+    eye_right_edge = eye_parts[0].x
+
+    eye_left_part = img[eye_parts[1].y:eye_parts[2].y, (2*eye_left_edge + eye_right_edge)//3:eye_left_edge]
+    eye_right_part = img[eye_parts[1].y:eye_parts[2].y, eye_right_edge:(eye_left_edge + 2*eye_right_edge)//3]
+
+    eye_img = img[eye_parts[1].y:eye_parts[2].y, eye_parts[0].x:eye_parts[3].x]
+    _, eye_left_part = cv2.threshold(cv2.cvtColor(eye_left_part, cv2.COLOR_RGB2GRAY), get_median_value_from_img(eye_img), 255, cv2.THRESH_BINARY_INV)
+    _, eye_right_part = cv2.threshold(cv2.cvtColor(eye_right_part, cv2.COLOR_RGB2GRAY), get_median_value_from_img(eye_img), 255, cv2.THRESH_BINARY_INV)
+
+    black_eye_occupancy_rate_threshold = 80
+    if calc_white_area(eye_left_part) > black_eye_occupancy_rate_threshold:
+        return LEFT
+    elif calc_white_area(eye_right_part) > black_eye_occupancy_rate_threshold:
+        return RIGHT
+    else:
+        return STRAIGHT
+
+def is_face_moving(parts_pre, parts):
+    sum = 0
+    for i in range(68):
+        add = np.sqrt((parts_pre[i].x - parts[i].x) ** 2 + (parts_pre[i].y - parts[i].y) ** 2)
+        sum = sum + add
+
+    moving_threshold = 500
+    if sum > moving_threshold:
+        print('You are MOVING.')
+        return True
+    else:
+        return False
+
+def show_img(img, parts, eye):
+    for i in range(2):
+        cv2.circle(img, eye[i], 3, (255, 255, 0), -1)
 
     for i in parts:
         cv2.circle(img, (i.x, i.y), 3, (255, 0, 0), -1)
@@ -127,29 +127,22 @@ while True:
     if len(dets) > 0:
         parts = predictor(frame, dets[0]).parts()
 
-        left_eye = eye_point(frame, parts)
-        right_eye = eye_point(frame, parts, False)
-
-        if count > 1:
-            sum = 0
-            for i in range(68):
-                add = np.sqrt((parts_pre[i].x - parts[i].x) ** 2 + (parts_pre[i].y - parts[i].y) ** 2)
-                sum = sum + add
-            if sum > 500:
-                moventFlag = 1
-                print('movement flag is true')
-            else:
-                movementFlag = 0
+        if count > 1: 
+            face_moving = is_face_moving(parts_pre, parts)
         else:
-            movementFlag = 1
+            face_moving = False    
 
-        if movementFlag != 1:
-            if eye_ratio(frame, parts) == 1 and eye_ratio(frame, parts, False) == 1:
-                print('Eye movement to the left')
-            elif eye_ratio(frame, parts) == 2 and eye_ratio(frame, parts, False) == 2:
-                print('Eye movement to the right')
+        if face_moving != True:
+            if eye_moving_direction(frame, parts) == LEFT and eye_moving_direction(frame, parts, False) == LEFT:
+                print('Eye movement to the LEFT')
+            if eye_moving_direction(frame, parts) == RIGHT and eye_moving_direction(frame, parts, False) == RIGHT:
+                print('Eye movement to the RIGHT')
+            if eye_moving_direction(frame, parts) == RIGHT and eye_moving_direction(frame, parts, False) == LEFT:
+                print('Eye CROSSING movement is detected!!')
 
-        p(frame, parts, (left_eye, right_eye))
+        left_eye = get_eye_center(frame, parts)
+        right_eye = get_eye_center(frame, parts, False)
+        show_img(frame, parts, (left_eye, right_eye))
 
         parts_pre = parts
         count = count + 1
