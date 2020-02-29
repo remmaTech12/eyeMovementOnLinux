e10617c9d1279cbd14382b6456991a10dab0e96c
renma <titech.renma@gmail.com>
2020-02-29 11:20:51 +0900

    Accuracy of detecting eye movement is improved by a modification of measuring eye ratio algorithm.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import dlib
 import cv2
-import statistics
 import numpy as np
+
 cv2.destroyAllWindows()
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
 
 def get_center(gray_img):
     moments = cv2.moments(gray_img, False)
@@ -15,18 +14,15 @@ def get_center(gray_img):
     except:
         return None
 
-
 def is_close(y0, y1):
-    if abs(y0 - y1) < 5:
+    if abs(y0 - y1) < 10:
         return True
     return False
-
 
 def matrix2line(arr):
     m = np.shape(arr)[0]
     n = np.shape(arr)[1]
     return arr.reshape(m*n,1)
-
 
 def eye_point(img, parts, left=True):
     if left:
@@ -47,7 +43,6 @@ def eye_point(img, parts, left=True):
         return None
 
     eye = img[eyes[1].y:eyes[2].y, eyes[0].x:eyes[3].x]
-    #eye = img[eyes[0].x:eyes[3].x, eyes[1].y:eyes[2].y]
     imgGrayLineData = matrix2line(cv2.cvtColor(eye, cv2.COLOR_RGB2GRAY))
     threshVal = np.median(imgGrayLineData)
     _, eye = cv2.threshold(cv2.cvtColor(eye, cv2.COLOR_RGB2GRAY), threshVal, 255, cv2.THRESH_BINARY_INV)
@@ -61,16 +56,9 @@ def eye_point(img, parts, left=True):
 def calc_black_whiteArea(bw_image):
     image_size = bw_image.size
     whitePixels = cv2.countNonZero(bw_image)
-    blackPixels = bw_image.size - whitePixels
- 
     whiteAreaRatio = (whitePixels/image_size)*100#[%]
 
-    #blackAreaRatio = (blackPixels/image_size)*100#[%]
-    ##print("White Area [%] : ", whiteAreaRatio)
-    #print("Black Area [%] : ", blackAreaRatio)
-
     return whiteAreaRatio
-
 
 def eye_ratio(img, parts, left=True):
     if left:
@@ -90,9 +78,11 @@ def eye_ratio(img, parts, left=True):
     if is_close(eyes[1].y, eyes[2].y):
         return None
 
+    redge_x = eyes[0].x
+    ledge_x = eyes[3].x
     eye = img[eyes[1].y:eyes[2].y, eyes[0].x:eyes[3].x]
-    eye_left_part = img[eyes[1].y:eyes[2].y, (eyes[3].x + eyes[0].x)//2:eyes[3].x]
-    eye_right_part = img[eyes[1].y:eyes[2].y, eyes[0].x:(eyes[3].x + eyes[0].x)//2]
+    eye_left_part = img[eyes[1].y:eyes[2].y, (2*ledge_x + redge_x)//3:ledge_x]
+    eye_right_part = img[eyes[1].y:eyes[2].y, redge_x:(ledge_x + 2*redge_x)//3]
 
     imgGrayLineData = matrix2line(cv2.cvtColor(eye, cv2.COLOR_RGB2GRAY))
     threshVal = np.median(imgGrayLineData)
@@ -108,14 +98,13 @@ def eye_ratio(img, parts, left=True):
         cv2.imshow("right_eye", eye)
     """
 
-    threshBW = 70
+    threshBW = 80
     if calc_black_whiteArea(eye_left_part) > threshBW:
         return 1
     elif calc_black_whiteArea(eye_right_part) > threshBW:
         return 2
     else:
         return 0
-
 
 def p(img, parts, eye):
     if eye[0]:
@@ -134,12 +123,9 @@ count = 1
 while True:
     ret, frame = cap.read()
 
-    #parts_pre = np.zeros(68)
     dets = detector(frame[:, :, ::-1])
     if len(dets) > 0:
         parts = predictor(frame, dets[0]).parts()
-        #print(np.shape(parts))
-        #print(parts[0])
 
         left_eye = eye_point(frame, parts)
         right_eye = eye_point(frame, parts, False)
@@ -149,7 +135,7 @@ while True:
             for i in range(68):
                 add = np.sqrt((parts_pre[i].x - parts[i].x) ** 2 + (parts_pre[i].y - parts[i].y) ** 2)
                 sum = sum + add
-            if sum > 100:
+            if sum > 500:
                 moventFlag = 1
                 print('movement flag is true')
             else:
@@ -159,27 +145,14 @@ while True:
 
         if movementFlag != 1:
             if eye_ratio(frame, parts) == 1 and eye_ratio(frame, parts, False) == 1:
-                print('Left eye movement')
+                print('Eye movement to the left')
             elif eye_ratio(frame, parts) == 2 and eye_ratio(frame, parts, False) == 2:
-                print('Right eye movement')
-        """
-        if left_eye != None and right_eye != None:
-            #print(abs(left_eye[0] - parts[36].x))    # for debugging
-            #print(abs(right_eye[0] - parts[42].x))   # for debugging
-            threshEyeVal = 15
-            if abs(left_eye[0] - parts[36].x) < threshEyeVal and abs(right_eye[0] - parts[42].x) < threshEyeVal:
-                print('Right eye movement')
-            if abs(left_eye[0] - parts[39].x) < threshEyeVal and abs(right_eye[0] - parts[45].x) < threshEyeVal:
-                print('Left eye movement')
-        """
+                print('Eye movement to the right')
 
-        p(frame * 0, parts, (left_eye, right_eye))
-        #p(frame, parts, (left_eye, right_eye))
+        p(frame, parts, (left_eye, right_eye))
 
         parts_pre = parts
-
         count = count + 1
-
 
     if cv2.waitKey(1) == 27:
         break
